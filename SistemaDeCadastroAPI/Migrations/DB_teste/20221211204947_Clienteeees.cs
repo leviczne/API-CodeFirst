405160267f8ee5da1a45a8597e40c4062b5811c3
Levi Santos <levi.santos@suporterenova.onmicrosoft.com>
2022-12-11 18:19:17 -0300

    funcionando

---
+++ SistemaDeCadastroAPI/Migrations/DB_teste/20221211204947_Clienteeees.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaDeCadastroAPI.Migrations.DB_teste
+{
+    public partial class Clienteeees : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    idCliente = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nchar(10)", fixedLength: true, maxLength: 10, nullable: true),
+                    Senha = table.Column<string>(type: "nchar(10)", fixedLength: true, maxLength: 10, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.idCliente);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pedidos",
+                columns: table => new
+                {
+                    idPedido = table.Column<int>(type: "int", nullable: false),
+                    idCliente = table.Column<int>(type: "int", nullable: false),
+                    Pedido = table.Column<string>(type: "nchar(10)", fixedLength: true, maxLength: 10, nullable: false),
+                    NumeroDoPedido = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedidos", x => x.idPedido);
+                    table.ForeignKey(
+                        name: "fk",
+                        column: x => x.idCliente,
+                        principalTable: "Clientes",
+                        principalColumn: "idCliente");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedidos_idCliente",
+                table: "Pedidos",
+                column: "idCliente");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pedidos");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
